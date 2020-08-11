641ffb7e3a8e8210c80684dc27a34eca7334c716
Luis Ramirez <lartkma@gmail.com>
2020-08-11 02:03:51 -0500

    Script to join many character gifs into one

---
+++ word2gif.sh
@@ -0,0 +1,24 @@
+#! /bin/sh -e
+
+if [ ${#1} != 0 ] && [ ${#1} = $(echo $1 | sed -e 's/\(.\)/\1\n/g' | xargs -I % find . -name %.gif | wc -l) ]; then
+  idx=0
+  echo $1 | sed -e 's/\(.\)/\1\n/g' | xargs -I % find . -name %.gif | while read gifpath; do
+    if [ $idx = 0 ]; then
+      cp $gifpath output.gif
+    else
+      convert \( \
+          output.gif[0] \
+          $gifpath[0] \
+          +smush -15 \
+		  -fuzz 30% -fill white -opaque red \
+		  -set delay 50 \
+        \) \
+        \( output.gif -delete 0 \( +clone -set delay 20 \) -delete -2 \) \
+        \( $gifpath -delete 0 -repage +$((135*idx))+0\! \) \
+        output.gif      
+    fi
+    idx=$((idx+1))
+  done
+else
+  echo Error: not all characters found in image list
+fi
